64b969eed00e918041b8ed547e6eda328a17f84e
EduardoCelentano <eduardo.celentano@sptech.school>
2025-11-20 10:36:57 -0300

    Atualização web-data-viz - Começo Dashboard

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -4,6 +4,7 @@ comandos para mysql server
 */
 
 
+
 /*
 comandos para mysql server
 */
@@ -15,7 +16,7 @@ CREATE TABLE usuario (
     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45),
     email VARCHAR(45),
-    dtNasc datetime,
+    dtNasc DATE,
     senha CHAR(8)
 );
 
@@ -36,10 +37,12 @@ INSERT INTO livros (nome, dt_lancamento, genero, qtd_pagina) VALUES
 ('xeque-mate', '2023-01-01', 'Romance', 336);
 
 CREATE TABLE avaliacao_usuario (
-    id_Livros INT,
+    id_Avaliacao INT,
+    id_Livros INT, 
     id_usuario INT,
     comentario VARCHAR(100),
-    CONSTRAINT pkComposta PRIMARY KEY (id_Livros, id_usuario),
+    avaliacao INT, 
+    CONSTRAINT pkComposta PRIMARY KEY (id_Avaliacao, id_Livros, id_usuario),
     CONSTRAINT livro_avaliacao FOREIGN KEY (id_Livros) REFERENCES livros(idLivros),
     CONSTRAINT usuario_avaliacao FOREIGN KEY (id_usuario) REFERENCES usuario(idUsuario)
 );
